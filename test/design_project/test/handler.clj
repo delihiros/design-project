8d029ea44ad6c0755020219b7e081d8b62c3a046
delihiros <delihiros@gmail.com>
2013-11-19 16:26:26 +0900

    initial commit

---
+++ test/design_project/test/handler.clj
@@ -0,0 +1,14 @@
+(ns design-project.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        design-project.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
