5ebc3e1280f7af105e3189361c0b4f36260c4601
naga-na <nagana00826@gmail.com>
2013-12-11 19:57:16 +0900

    write document event-type.clj

--- src/design_project/Models/event_type.clj
+++ src/design_project/Models/event_type.clj
@@ -1,17 +1,24 @@
-(ns design-project.Models.event_type
-	(:use [design-project.Models.database])
-	(:require [clojure.java.jdbc :as jdbc]))
+(ns design-project.Models.event-type
+  (:use [design-project.Models.database])
+  (:require [clojure.java.jdbc :as jdbc]))
 
-;; insert
-(defn insert [event]
-	(jdbc/insert! my-db :event_type
-		{:name (:name event)}))
+;;insert
+(defn insert
+ "insert event-type table.
+ parameter
+  event-type-map attribute
+   :name 
+ return 
+  generate id"
+  [event-type-map]
+  (jdbc/insert! my-db :event_type event-type-map))
 
 ;; select
-(defn db-select []
-	(jdbc/query my-db
-		["select * from event_type"]))
+(defn db-select 
+  "select from event-type table.
+  return
+   select data in map."
+  []
+  (jdbc/query my-db
+              ["select * from event_type"]))
 
-(insert {:name "s"})
-
-(db-select)
