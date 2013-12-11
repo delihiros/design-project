24822fb38b7f0f2de9108fcd041e3b0675b2983b
naga-na <nagana00826@gmail.com>
2013-12-11 14:25:21 +0900

    add document in company.clj

--- src/design_project/Models/company.clj
+++ src/design_project/Models/company.clj
@@ -6,17 +6,34 @@
 ;; 欲しいデータを取り出しやすくする
 ;; ちゃんと値のチェックもする
 
-
 ;; insert
-(defn insert [com]
-  (jdbc/insert! my-db :company
-                {:name (:name com)}))
+(defn insert 
+  "insert company table.
+  com-map attribute 
+   :name company name.
+  return 
+   generate id"
+  [com-map]
+  (jdbc/insert! my-db :company com-map))
+
+;; update
+(defn update [id com-map]
+  (jdbc/update! my-db :company com-map ["id=?" id]))
 
 ;; select
-(defn db-select []
+;; filter かけれるようにする予定
+(defn db-select 
+  "select from company table.
+  return
+   select data in map."
   (jdbc/query my-db
               ["select * from company"]))
 
 (insert {:name "t"})
 
-( select)
+(db-select)
+
+
+(update 1 {:name "update"})
+
+
--- src/design_project/Models/user.clj
+++ src/design_project/Models/user.clj
@@ -10,6 +10,7 @@
 ;; insert
 ;; 大学id
 ;; ユーザー状態
+;; イメージ画像
 ;; 氏名
 ;; パスワード
 ;; ハッシュ
@@ -35,39 +36,23 @@
 ;; 保証人電話番号
 ;; 修了日
 (defn insert [user-set]
-  (jdbc/insert! my-db :user
-                {:university_id (:university_id user-set),
-                 :status (:status user-set),
-                 :name (:name user-set),
-                 :password (:password user-set),
-                 :hash_method (:hash_method user-set),
-                 :country (:country user-set),
-                 :address (:address user-set),
-                 :phone (:phone user-set),
-                 :job_id (:job_id user-set),
-                 :industry_id (:industry_id user-set),
-                 :birthday (:birthday user-set),
-                 :sex (:sex user-set),
-                 :wish_curriculum (:wish_curriculum user-set),
-                 :wish_course_id (:wish_course_id user-set),
-                 :wish_teacher (:wish_teacher user-set),
-                 :entrance (:entrance user-set),
-                 :entrance_day (:entrance_day user-set),
-                 :class (:class user-set),
-                 :department (:department user-set),
-                 :graduate_course (:graduate_course user-set),
-                 :laboratory_id (:laboratory_id user-set),
-                 :student_id (:student_id user-set),
-                 :guarantor_name (:guarantor_name user-set),
-                 :guarantor_address (:guarantor_address user-set),
-                 :guarantor_phone (:guarantor_phone user-set),
-                 :finish_cource_day (:finish_cource_day user-set)}))
+  (jdbc/insert! my-db :user user-set))
+
+(defn update 
+  "update user table.
+  parameter
+   want update id and change attribute.
+  return
+   generate id."
+  [id user-set]
+  (jdbc/update! my-db :company user-set ["id=?" id]))
+
+
 
 ;; select
 (defn db-select []
   (jdbc/query my-db
               ["select * from user"]))
 
-(insert {:name "t"})
 
 
