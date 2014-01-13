c95c026ec97de1f0bed86890ff9d2c3236163f62
naga-na <nagana00826@gmail.com>
2014-01-13 21:06:06 +0900

    add user id

--- src/design_project/sql/V1__create_user_table.sql
+++ src/design_project/sql/V1__create_user_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 	`status` INT NOT NULL,
 	`image` TEXT,
 	`name` VARCHAR(32) NOT NULL,
+	`user_id` TEXT UNIQUE NOT NULL,
 	`password` VARCHAR(255) NOT NULL,
 	`hash_method` VARCHAR(255) NOT NULL,
 	`country` VARCHAR(32) NOT NULL,
@@ -39,14 +40,14 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 ENGINE = InnoDB;
 
-insert into `user` (`id`, `university_id`, `status`, `image`, `name`, `password`, `hash_method`, `country`, `address`, `phone`, `job_id`, `industry_id`, `birthday`, `sex`)  values(null, 1, 0, null, 'admin', 'admin', 'admin', '国3', '住所2', '0000000000', 1, 1, '1960-03-24', 1);
+insert into `user` (`id`, `university_id`, `status`, `image`, `name`, `user_id` ,`password`, `hash_method`, `country`, `address`, `phone`, `job_id`, `industry_id`, `birthday`, `sex`)  values(null, 1, 0, null, 'admin', 'admin', 'admin', 'admin', '国3', '住所2', '0000000000', 1, 1, '1960-03-24', 1);
 
-insert into `user` values(null, 1, 1, null, '名前', 'password', 'password_hash', '国1', '住所1', '0120333333',
+insert into `user` values(null, 1, 1, null, '名前', 'student', 'password', 'password_hash', '国1', '住所1', '0120333333',
 	1, 1, '1993-03-09', 1, 1, 1, '先生1', true, '2010-04-01', 3, 1, 1, 1, '11t5000a', '保証人', '保証人住所', '0120111111', null);
 
-insert into `user` (`id`, `university_id`, `status`, `image`, `name`, `password`, `hash_method`, `country`, `address`, `phone`, `job_id`, `industry_id`, `birthday`, `sex`) values(null, 1, 2, null, 'entry', 'entry', 'entry', '国2', '住所1', '0001000000', 1, 1, '1995-07-04', 1);
+insert into `user` (`id`, `university_id`, `status`, `image`, `name`, `user_id`, `password`, `hash_method`, `country`, `address`, `phone`, `job_id`, `industry_id`, `birthday`, `sex`) values(null, 1, 2, null, 'entry', 'entry', 'entry', 'entry', '国2', '住所1', '0001000000', 1, 1, '1995-07-04', 1);
 
-insert into `user` values(null, 1, 3, null, 'graduated', 'graduated', 'graduated', '国2', '住所1', '0001000000',
+insert into `user` values(null, 1, 3, null, 'graduated', 'graduated', 'graduated', 'graduated', '国2', '住所1', '0001000000',
 	1, 1, '1990-07-04', 1, 1, 1, '先生2', true, '2007-04-01', null, 1, 1, 1, '07a0000a', 'ほしょうにん', 'じゅうしょ', '1234567890', '2012-03-31');
 
 
