2c2e4c17291ee11d5b0a47573bd396bd98a69335
naga-na <nagana00826@gmail.com>
2014-01-14 22:09:17 +0900

    fix date

--- src/design_project/Models/database.clj
+++ src/design_project/Models/database.clj
@@ -3,7 +3,7 @@
 ;; ユーザー名とパスワードは各々の環境にあわせて変更する
 ;;
 (def my-db {:subprotocol "mysql"
-            :subname "//127.0.0.1:3306/t"
+            :subname "//127.0.0.1:3306/t?zeroDateTimeBehavior=convertToNull"
             :user "user"
             :password "password"})
 
